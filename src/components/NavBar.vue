7d2b161430ecfa0f328d164bc751a4d71ab255b5
Ramiro Garces <ramgarces24@gmail.com>
2023-02-07 01:35:19 -0600

    Add style to nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-const brand = ref('🏛️ Fake Company Directory')
+const brand = ref('♟️ Chess Connections')
 </script>
 
 <template>
@@ -20,22 +20,22 @@ const brand = ref('🏛️ Fake Company Directory')
 
 <style scoped lang="postcss">
 nav {
-  @apply flex h-20 bg-slate-900 text-slate-200;
+  @apply flex h-20 bg-neutral-900 text-slate-200;
   .wrapper {
     @apply container mx-auto flex w-full items-center justify-between;
     .brand {
       &-title {
-        @apply text-2xl font-bold text-yellow-500;
+        @apply text-2xl font-bold text-slate-100;
       }
     }
     .menu {
       @apply flex gap-2;
       &-item {
-        @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+        @apply rounded-md px-4 py-2 hover:bg-green-600 hover:text-slate-900;
       }
       &-login {
-        @apply rounded-md bg-red-500 px-4 py-2 hover:bg-red-700
-        hover:text-red-100;
+        @apply rounded-md bg-slate-500 px-4 py-2 hover:bg-red-700
+        hover:text-slate-100;
       }
     }
   }
