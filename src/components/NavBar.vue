c5fca2644236f049c03933bd6d312dc152bd8114
Ramiro Garces <ramgarces24@gmail.com>
2023-02-06 22:32:01 -0600

    Add styling for menu links and logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@ const brand = ref('🏛️ Fake Company Directory')
       <div class="menu">
         <a href="#" class="menu-item">Departments</a>
         <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-item">Logout</a>
+        <a href="#" class="menu-login">Logout</a>
       </div>
     </div>
   </nav>
@@ -23,6 +23,21 @@ nav {
   @apply flex h-20 bg-slate-900 text-slate-200;
   .wrapper {
     @apply container mx-auto flex w-full items-center justify-between;
+    .brand {
+      &-title {
+        @apply text-2xl font-bold text-yellow-500;
+      }
+    }
+    .menu {
+      @apply flex gap-2;
+      &-item {
+        @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+      }
+      &-login {
+        @apply rounded-md bg-red-500 px-4 py-2 hover:bg-red-700
+        hover:text-red-100;
+      }
+    }
   }
 }
 </style>
