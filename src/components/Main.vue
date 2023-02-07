ddc9dd43b510ac84c302f24fddb237c0ac71ce3c
Ramiro Garces <ramgarces24@gmail.com>
2023-02-06 22:55:34 -0600

    Add search bar and styling

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import MainSearch from './MainSearch.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <MainSearch />
+    <h1>Hello There</h1>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.wrapper {
+  @apply container mx-auto flex flex-col gap-4 py-4;
+}
+</style>
