c3827607bb003e6a4d16c540aa4f9944a2a49d96
Ramiro Garces <ramgarces24@gmail.com>
2023-02-07 01:21:41 -0600

    Add pagination

---
+++ src/components/MainPagination.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+const pages = ref(10)
+const activePage = ref(4)
+
+const prevPage = () => {
+  if (activePage.value > 1) {
+    activePage.value--
+  }
+}
+
+const nextPage = () => {
+  if (activePage.value < pages.value) {
+    activePage.value++
+  }
+}
+</script>
+
+<template>
+  <div class="pagination">
+    <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
+    <button v-for="page in pages" class="page" :class="page === activePage ? 'active' : ''" @click="activePage = page">
+      {{ page }}
+    </button>
+    <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.pagination {
+  @apply flex justify-center gap-4;
+  .action {
+    @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md
+    hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+  }
+  .page {
+    @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md
+    hover:bg-slate-200;
+    &.active {
+      @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
+    }
+  }
+}
+</style>
