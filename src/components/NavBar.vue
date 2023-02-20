a07135398a9c056e2b4dcc7abf3f671834afa279
Ramiro Garces <ramgarces24@gmail.com>
2023-02-20 09:35:29 -0600

    Add Dev Env variable and add to NavBar.vue file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@ import { useAuth } from '@/composables/useAuth'
 
 const { isAuthenticated, logout, user } = useAuth()
 
-const brand = ref('♟️ Chess Connections')
+const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
