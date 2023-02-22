9bf9f8e1b71dca88d12c395dd798bcda46e0f06a
Ramiro Garces <ramgarces24@gmail.com>
2023-02-22 09:32:54 -0600

    Add Firebase configuration for email auth

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -11,8 +11,8 @@ const route = useRoute()
 const username = ref('')
 const password = ref('')
 
-const logUserIn = () => {
-  if (login(username.value, password.value)) {
+const logUserIn = async () => {
+  if (await login(username.value, password.value)) {
     if (route.query.redirect) {
       router.push(route.query.redirect)
     } else {
