fdfba9f0e25acbcd94241e93b13f47af37dd1d93
Ramiro Garces <ramgarces24@gmail.com>
2023-02-22 23:35:57 -0600

    Add firebase configuration for email auth

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,7 +17,7 @@ const brand = ref(import.meta.env.VITE_APP_NAME)
         <p v-show="isAuthenticated" class="px-2 py-4">
           Welcome back
           <strong class="text-green-800"
-            ><i>{{ user.name }}</i></strong
+            ><i>{{ user?.email }}</i></strong
           >
         </p>
         <div v-if="isAuthenticated">
